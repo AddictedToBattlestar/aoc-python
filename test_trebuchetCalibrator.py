39d25aa907a1fd5da2d86bf9e9e03e607d9c7700
AddictedToBattlestar <addictedtobsg@gmail.com>
2023-12-01 20:27:05 -0500

    Day 1, part 2. Tests pass. Answer is not correct.

--- test_trebuchetCalibrator.py
+++ test_trebuchetCalibrator.py
@@ -1,13 +1,22 @@
 from unittest import TestCase
-from trebuchetCalibrator import get_value, calculate, calculate_from_file
+from trebuchetCalibrator import get_calibration_value, calculate, calculate_from_file
 
 
 class TestTrebuchetCalibrator(TestCase):
     def test_get_value(self):
-        self.assertEqual(12, get_value(line="1abc2"))
-        self.assertEqual(38, get_value(line="pqr3stu8vwx"))
-        self.assertEqual(15, get_value(line="a1b2c3d4e5f"))
-        self.assertEqual(77, get_value(line="treb7uchet"))
+        self.assertEqual(12, get_calibration_value(line="1abc2"))
+        self.assertEqual(38, get_calibration_value(line="pqr3stu8vwx"))
+        self.assertEqual(15, get_calibration_value(line="a1b2c3d4e5f"))
+        self.assertEqual(77, get_calibration_value(line="treb7uchet"))
+
+    def test_get_value_with_numbers_spelled(self):
+        self.assertEqual(29, get_calibration_value(line="two1nine"))
+        self.assertEqual(83, get_calibration_value(line="eightwothree"))
+        self.assertEqual(13, get_calibration_value(line="abcone2threexyz"))
+        self.assertEqual(24, get_calibration_value(line="xtwone3four"))
+        self.assertEqual(42, get_calibration_value(line="4nineeightseven2"))
+        self.assertEqual(14, get_calibration_value(line="zoneight234"))
+        self.assertEqual(76, get_calibration_value(line="7pqrstsixteen"))
 
     def test_calculate(self):
         self.assertEqual(142, calculate(lines=[
@@ -19,3 +28,6 @@ class TestTrebuchetCalibrator(TestCase):
 
     def test_calculate_from_file(self):
         self.assertEqual(142, calculate_from_file(file_name="day1-part1-sample-data.txt"))
+
+    def test_calculate_from_file_part2(self):
+        self.assertEqual(281, calculate_from_file(file_name="day1-part2-sample-data.txt"))
--- trebuchetCalibrator.py
+++ trebuchetCalibrator.py
@@ -1,18 +1,46 @@
-def get_value(line):
+numbers = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9
+}
+
+
+def get_calibration_value(line):
     left = None
     right = None
+    buffer = ""
     for c in line:
-        if c.isdigit():
+        value = get_value(c, buffer)
+        if value:
             if not left:
-                left = int(c)
-            right = int(c)
+                left = value
+            right = value
+            buffer = ""
+        else:
+            buffer = buffer + c
     return (left * 10) + right
+
+
+def get_value(current_character, buffer):
+    if current_character.isdigit():
+        return int(current_character)
+    new_buffer = buffer + current_character
+    for number_string, value in numbers.items():
+        if number_string == new_buffer[-len(number_string):]:
+            return value
+    return None
 
 
 def calculate(lines):
     calibration_value = 0
     for line in lines:
-        calibration_value += get_value(line=line)
+        calibration_value += get_calibration_value(line=line)
     return calibration_value
 
 
@@ -23,5 +51,7 @@ def calculate_from_file(file_name):
 
 
 if __name__ == '__main__':
-    result = calculate_from_file(file_name="day1-part1-data.txt")
-    print(f'The solution for Day 1, part 1 is: {result}')
+    result = calculate_from_file(file_name="day1-data.txt")
+    print(f'The solution for Day 1 is: {result}')
+    # part 1: 54159
+    # part 2: 53900, too high
