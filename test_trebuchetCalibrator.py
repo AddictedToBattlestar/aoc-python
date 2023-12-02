26c02990a8039b630195abf022d05769ec48dee4
AddictedToBattlestar <addictedtobsg@gmail.com>
2023-12-01 19:50:40 -0500

    Day 1, part 1

--- test_trebuchetCalibrator.py
+++ test_trebuchetCalibrator.py
@@ -1,6 +1,21 @@
 from unittest import TestCase
-from trebuchetCalibrator import get_value
+from trebuchetCalibrator import get_value, calculate, calculate_from_file
+
 
 class TestTrebuchetCalibrator(TestCase):
     def test_get_value(self):
-        self.assertEquals(get_value("1abc2"), 12)
+        self.assertEqual(12, get_value(line="1abc2"))
+        self.assertEqual(38, get_value(line="pqr3stu8vwx"))
+        self.assertEqual(15, get_value(line="a1b2c3d4e5f"))
+        self.assertEqual(77, get_value(line="treb7uchet"))
+
+    def test_calculate(self):
+        self.assertEqual(142, calculate(lines=[
+            "1abc2",
+            "pqr3stu8vwx",
+            "a1b2c3d4e5f",
+            "treb7uchet"
+        ]))
+
+    def test_calculate_from_file(self):
+        self.assertEqual(142, calculate_from_file(file_name="day1-part1-sample-data.txt"))
--- trebuchetCalibrator.py
+++ trebuchetCalibrator.py
@@ -1,2 +1,27 @@
-def get_value(calibrationLine):
-    return 0
+def get_value(line):
+    left = None
+    right = None
+    for c in line:
+        if c.isdigit():
+            if not left:
+                left = int(c)
+            right = int(c)
+    return (left * 10) + right
+
+
+def calculate(lines):
+    calibration_value = 0
+    for line in lines:
+        calibration_value += get_value(line=line)
+    return calibration_value
+
+
+def calculate_from_file(file_name):
+    text_file = open(file_name, "r")
+    lines = text_file.readlines()
+    return calculate(lines=lines)
+
+
+if __name__ == '__main__':
+    result = calculate_from_file(file_name="day1-part1-data.txt")
+    print(f'The solution for Day 1, part 1 is: {result}')
